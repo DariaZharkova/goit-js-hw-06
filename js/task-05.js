b85b2a632decd87ba36dc9f38278549a48e71365
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2023-11-05 20:26:58 +0200

    add task-05

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
+
+inputName.addEventListener("input", onInput);
+
+function onInput(evt) {
+  evt.currentTarget.value.trim() !== ""
+    ? (outputName.textContent = evt.currentTarget.value.trim())
+    : (outputName.textContent = "Anonymous");
+}
