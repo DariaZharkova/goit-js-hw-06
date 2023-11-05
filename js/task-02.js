868817d75dbfd192f86a178935c420f7a5ed5dc7
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2023-11-05 15:30:50 +0200

    add task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+function createMarkup(arr) {
+  const markup = arr.map((element) => {
+    const li = document.createElement("li");
+    li.textContent = element;
+    li.classList.add("item");
+    return li;
+  });
+  list.append(...markup);
+}
+
+createMarkup(ingredients);
