dcc807512c7be02f952e40233bc1359526db56d2
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2023-11-10 23:00:43 +0200

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+  if (email.value === "" || password.value === "") {
+    alert("Please, fill in all fields!");
+  } else {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+  }
+  evt.currentTarget.reset();
+}
